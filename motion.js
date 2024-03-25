58c9c12019f52021b63bb3e94b2891ad63ed3a8d
Ronit Dutta <96005252+WebRonit@users.noreply.github.com>
2024-03-25 19:26:12 +0530

    added 3d hover and some fixes

--- motion.js
+++ motion.js
@@ -1,7 +1,7 @@
 var text = document.querySelectorAll(".animate-text");
 text.forEach((text) => {
        var textToAnim = text.innerText;
-       var delay = 100;
+       var delay = text.dataset.speed || 100;
        let i = 0;
        text.textContent = '';
 
@@ -22,29 +22,30 @@ text.forEach((text) => {
 
 
 var hoverBoxes = document.querySelectorAll(".hover-3d");
-hoverBoxes.forEach((hoverBoxes) => {
+hoverBoxes.forEach((hoverBox) => {
    
-    let height = hoverBoxes.clientHeight;
-    let width = hoverBoxes.clientWidth;
+    let height = hoverBox.clientHeight;
+    let width = hoverBox.clientWidth;
+    let tilt = hoverBox.dataset.tilt || 20;
 
-    hoverBoxes.style.transition = "transform .1s";
+    hoverBox.style.transition = "transform .1s";
 
-    hoverBoxes.addEventListener('mousemove', (e) => {
+    hoverBox.addEventListener('mousemove', (e) => {
    
         var posX = e.layerX;
         var posY = e.layerY;
     
-        var rx = Math.floor( 25 * ((posX - width / 2) / width));
-        var ry = Math.floor( -25 * ((posY - height / 2) / height));
+        var rx = Math.floor( tilt * ((posX - width / 2) / width));
+        var ry = Math.floor( -tilt * ((posY - height / 2) / height));
     
-        var boxParent = hoverBoxes.parentNode;
-        boxParent.style.perspective = "800px"
-        hoverBoxes.style.transform = "rotateX(" + ry + "deg) rotateY(" + rx + "deg)";
+        var boxParent = hoverBox.parentNode;
+        boxParent.style.perspective = "1000px"
+        hoverBox.style.transform = "rotateX(" + ry + "deg) rotateY(" + rx + "deg)";
 
-        hoverBoxes.addEventListener('mouseout', () => {
-             hoverBoxes.style.transform = "rotateX(0deg) rotateY(0deg)"
+        hoverBox.addEventListener('mouseout', () => {
+             hoverBox.style.transform = "rotateX(0deg) rotateY(0deg)"
         });
    
     });
-})
+});
 
